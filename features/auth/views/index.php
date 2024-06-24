81ca86a18f2817a412baa7274b5366a4cfc5cecd
unknown <mhdfarhanl6267@gmail.com>
2024-06-25 05:34:28 +0700

    style(features): update lyaout & color for splash & login

--- features/auth/views/index.php
+++ features/auth/views/index.php
@@ -6,32 +6,112 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Overlogic</title>
   <link href="/features/auth/styles/style.css" rel="stylesheet" />
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <link rel="shortcut icon" href="../../../assets/logog.png" type="image/x-icon">
+  <style>
+    body, html {
+      margin: 0;
+      padding: 0;
+      height: 100%;
+    }
+    .SplashPage {
+      width: 100%;
+      min-height: 100vh;
+      background-image: url('../../../assets/bg_lp.png');
+      display: flex;
+      flex-direction: column;
+    }
+    .content-wrapper {
+      display: flex;
+      flex-grow: 1;
+      justify-content: center;
+      align-items: center;
+      padding: 2rem;
+      flex-direction: column;
+    }
+    .content-box {
+      background: white;
+      border-radius: 25px;
+      padding: 2rem;
+      display: flex;
+      max-width: 1200px;
+      width: 100%;
+    }
+    .left-content {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+    .right-content {
+      flex: 1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .title {
+      color: #242424;
+      font-size: 50px;
+      font-weight: 600;
+      line-height: 49px;
+      margin-bottom: 1rem;
+    }
+    .subtitle {
+      color: #242424;
+      font-size: 20px;
+      font-weight: 700;
+      line-height: 25px;
+      margin-bottom: 2rem;
+    }
+    .login-button {
+      background: #0F1322;
+      border-radius: 15px;
+      color: #D9D9D9;
+      font-size: 21px;
+      font-weight: 600;
+      text-decoration: none;
+      padding: 1rem 2rem;
+      display: inline-block;
+    }
+    .image-wrapper img {
+      max-width: 100%;
+      height: auto;
+      border-radius: 10px;
+    }
+  </style>
 </head>
 
 <body>
-  <div class="SplashPage" style="width: 1280px; height: 832px; position: relative">
-    <div class="Rectangle4" style="width: 1280px; height: 832px; left: -0px; top: 0px; position: absolute; background: #0F1322"></div>
-    <img class="Image22" style="width: 1500px; height: 837px; left: -19px; top: -5px; position: absolute" src="../../../assets/bg_lp.png" />
-    <div class="Group290" style="width: 297px; height: 54px; left: -10px; top: 32px; position: absolute">
-      <div class="OverloIc" style="width: 290px; height: 50px; left: 10px; top: 5px; position: absolute; text-align: center; color: white; font-size: 34px; font-weight: 700; line-height: 15px; word-wrap: break-word"><img src="../../../assets/Logo Tulisan.png" alt="Company Logo"> </div>
-    </div>
-    <div class="Line6" style="width: 103px; height: 0px; left: 350px; top: 252px; position: absolute; border: 3px #242424 solid"></div>
-    <div class="Group295" style="width: 1200px; height: 538px; left: 40px; top: 39px; position: absolute">
-      <div class="Group294" style="width: 1200px; height: 538px; left: 0px; top: 0px; position: absolute">
-        <div class="Rectangle300" style="width: 1200px; height: 484px; left: 0px; top: 54px; position: absolute; background: white; border-radius: 25px"></div>
-
+  <div class="SplashPage">
+    <div class="Group290" data-aos="fade-right" style="margin: 1rem;">
+      <div class="OverloIc">
+        <img src="../../../assets/Logo Tulisan.png" alt="Company Logo">
       </div>
-      <div class="EmployeeManagementSystem" style="width: 400px; height: 130px; left: 50px; top: 171px; position: absolute; color: #242424; font-size: 50px; font-weight: 600; line-height: 49px; word-wrap: break-word">EMPLOYEE <br />MANAGEMENT SYSTEM</div>
-      <div class="CompanyManagementSystemViaDatabaseWelcomeAdmin" style="width: 261px; height: 55px; left: 55px; top: 332px; position: absolute"><span style="color: #242424; font-size: 15px;">Company management system <br /></span><span style="color: #242424; font-size: 15px;">via database</span>
-        <div class="WelcomeAdmin"><span style="color: #242424; font-size: 20px; font-weight: 700; line-height: 25px; word-wrap: break-word"><span>Welcome Admin !</span></div>
-        <div class="OverlogicManagementSystem" style="width: 261px; height: 15px; left: -17px; top: -175px; position: absolute; text-align: center; color: #242424; font-size: 13px;">OVERLOGIC MANAGEMENT SYSTEM</div>
-        <div class="Group293" style="width: 157px; height: 51px; left: 46px; top: 404px; position: absolute">
-          <div class="Rectangle301" style="width: 157px; height: 51px; left: -50px; top: -330px; position: absolute; background: #0F1322; border-radius: 15px"></div>
-          <a href="/login" class="LogIn" style="text-decoration:none; width: 89px; height: 17px; left: -15px; top: -315px; position: absolute; text-align: center; color: #D9D9D9; font-size: 21px; font-weight: 600; line-height: 15px; word-wrap: break-word">Log in</a>
+    </div>
+    <div class="content-wrapper">
+      <div class="content-box" data-aos="fade-up">
+        <div class="left-content">
+          <h1 class="title" data-aos="fade-up" data-aos-delay="300">EMPLOYEE <br />MANAGEMENT SYSTEM</h1>
+          <h3 data-aos="fade-up" data-aos-delay="300">For Overlogic Administrator</h3>
+          <p class="subtitle" data-aos="fade-up" data-aos-delay="400">Welcome !</p>
+          <a href="/login" class="login-button" data-aos="zoom-in" data-aos-delay="500">Log in</a>
+        </div>
+        <div class="right-content">
+          <div class="image-wrapper" data-aos="fade-left" data-aos-delay="600">
+            <img src="../../../assets/Discussion.png" alt="Discussion Image">
+          </div>
         </div>
       </div>
-      <img class="Image18" style="width: 464px; height: 309px; left: 700px; top: 150px; position: absolute; border-radius: 10px" src="../../../assets/Discussion.png" />
     </div>
+  </div>
+
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  </script>
 </body>
 
 </html>
--- features/auth/views/login.php
+++ features/auth/views/login.php
@@ -6,44 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Overlogic</title>
     <link href="/features/auth/styles/style.css" rel="stylesheet" />
+    <link rel="shortcut icon" href="../../../assets/logog.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" />
 </head>
-<script>
-    // menampilkan error dari session
-    window.onload = function() {
-        <?php
-        session_start();
-        if (isset($_SESSION['error'])) {
-            echo 'alert("' . addslashes($_SESSION['error']) . '");';
-            unset($_SESSION['error']); // hapus error message dari session
-        }
-        ?>
-    };
-</script>
 
 <body>
+    <script>
+        // menampilkan error dari session
+        window.onload = function () {
+            <?php
+session_start();
+if (isset($_SESSION['error'])) {
+    echo 'alert("' . addslashes($_SESSION['error']) . '");';
+    unset($_SESSION['error']); // hapus error message dari session
+}
+?>
+        };
+    </script>
 
     <div class="Awalan">
-        <div class="Group289">
-            <img class="background" src="../../../assets/Bg.png" alt="Background Image" />
-            <div class="Rectangle-Bg"></div>
+        <div class="Group289" data-aos="fade-up">
             <div class="Rectangle-Container"></div>
-            <img class="Image-WorkTeam" src="../../../assets/WorkTeam.png" alt="Work Team Image" />
-            <img class="Image-LogoOverLogic" src="../../../assets/LogoOverLogic.png" alt="Logo OverLogic Image" />
-            <div class="WelcomeBack">WELCOME BACK!</div>
-            <div class="Admin">ADMIN</div>
-            <form action="/login-process" method="POST">
+            <img class="Image-WorkTeam" src="../../../assets/WorkTeam.png" alt="Work Team Image" data-aos="fade-left" />
+            <img class="Image-LogoOverLogic" src="../../../assets/logog.png" alt="Logo OverLogic Image" data-aos="fade-right" />
+            <div class="WelcomeBack" data-aos="fade-up">WELCOME BACK!</div>
+            <div class="Admin" data-aos="fade-up" data-aos-delay="100">ADMIN</div>
+            <form action="/login-process" method="POST" data-aos="fade-up" data-aos-delay="200">
                 <div class="Email">Email</div>
                 <input type="email" name="email" class="Input-Container" placeholder="E.g. Overlogic@gmail.com" required>
-
                 <div class="Password">Password</div>
                 <input type="password" name="password" class="Input-Password-Container" placeholder="Enter Your Password" required>
                 <button type="submit" class="Login-Button-Container">Login</button>
             </form>
-            <?php if (isset($_GET['error'])) : ?>
-                <p class="ErrorMessage"><?php echo $_GET['error']; ?></p>
-            <?php endif; ?>
+            <?php if (isset($_GET['error'])): ?>
+                <p class="ErrorMessage" data-aos="fade-up" data-aos-delay="300"><?php echo $_GET['error']; ?></p>
+            <?php endif;?>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
 
 </html>
